8621d9c0ff4c119eb201b7ab1e7aac75d61a0de6
walker <walker@ctitech.cn>
2014-02-14 13:01:00 +0800

    add test proj again

---
+++ SdkTest/SinaTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartSnsPublisher;
+using SmartSnsPublisher.Service;
+
+namespace SdkTest
+{
+    [TestClass]
+    public class SinaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ SdkTest/SiteRepoTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartSnsPublisher.Web.Models;
+using SmartSnsPublisher.Web.Repository;
+
+namespace SdkTest
+{
+    [TestClass]
+    public class SiteRepoTest
+    {
+        private readonly SiteInfoRepository repo = new SiteInfoRepository();
+
+        [TestMethod]
+        public void TestGetList()
+        {
+            //repo.AddConnectSite(new SiteInfo
+            //{
+            //    AccessToken = "accesstoken",
+            //    Description = "des",
+            //    ExpireDate = DateTime.Now,
+            //    Id = 1,
+            //    SiteName = "sitename",
+            //    UserId = "aabc"
+            //});
+            //repo.AddConnectSite(new SiteInfo
+            //{
+            //    AccessToken = "accesstoken",
+            //    Description = "des",
+            //    ExpireDate = DateTime.Now,
+            //    Id = 1,
+            //    SiteName = "sitename2",
+            //    UserId = "aabc"
+            //});
+            //Console.WriteLine(repo.UserConnectedSites("aabc").Count());
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine(DateTime.UtcNow.ToString());
+        }
+    }
+}
